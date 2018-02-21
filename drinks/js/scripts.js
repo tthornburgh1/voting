81726d9dc6b75504ac95e451a9c11faf4c4a69ae
Tristan Thornburgh <tristan.thornburgh@gmail.com>
2018-02-20 20:31:36 -0600

    setting up a voting page with Javascript

---
+++ drinks/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  debugger;
+  var age = parseInt(prompt("How old are you?"));
+  
+  if (age > 18) {
+    $('#voting').show();
+  } else if (age === 18) {
+    alert("Congrats on becoming an adult!");
+    $('#voting').show();
+  } else {
+    $('#under-18').show();
+  }
+});
